0918ce9ed9df3e65ab5d622f6b5517a1f7051de6
daukadolt <daulet.amirkhanov.official@gmail.com>
2018-06-28 18:26:20 +0600

    MVP

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Bookmarks:
 2. https://stackoverflow.com/questions/8149569/scan-a-directory-to-find-files-in-c
 3. https://stackoverflow.com/questions/2910377/get-home-directory-in-linux-c
 4. https://medium.com/@xabaras/sending-a-message-to-a-telegram-channel-the-easy-way-eb0a0b32968
+
+**Compiling**:
+
+```
+gcc -o dauletTest main.c -lcurl
+```
+
+.channelConfig:
+```
+telegramBotToken
+@channelNameHere
+```
--- main.c
+++ main.c
@@ -3,29 +3,77 @@
 #include <sys/types.h>
 #include <pwd.h>
 #include <string.h>
+#include <curl/curl.h>
+#include <stdlib.h>
 
-int main() {
+
+int main(int argc, char **argv) {
 
     FILE *configFile;
 
     struct passwd *pw = getpwuid(getuid());
-
     char *homedir = pw->pw_dir;
-    char *test = "/Projects/myGithub/bookmarkTelegramChannel/.channelConfig";
+    char *directory = "/Projects/myGithub/bookmarkTelegramChannel/.channelConfig";
 
-    strcat(homedir, test);
 
+    char *text = (char*) malloc(100 * sizeof(char));
+    printf("Text: ");
+    scanf("%255s", text);
+
+
+    // for (char **a = argv ; a != argv+argc ; a++) {
+    //     for(char *p = *a ; *p != '\0' ; p++) {
+    //         // code uses *p instead of argv[i][j]
+    //         printf("%c", *p);
+    //     }
+    // }
+
+    strcat(homedir, directory);
     printf("%s\n", homedir);
-
     configFile = fopen(homedir, "r");
 
-    char token[45];
+    char token[100];
+    char channel_name[100];
 
     if(configFile) {
         fscanf(configFile, "%s", token);
+        fscanf(configFile, "%s", channel_name);
     }
 
-    printf("%s\n", token);
+    printf("%s\n%s\n",token, channel_name);
+
+// CURL
+
+    CURL *curl;
+    CURLcode res;
+
+    char URL[1000];
+    sprintf(URL, "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s", token, channel_name, text);
+    
+
+
+
+
+    printf("\n%s\n", URL);
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, URL);
+        /* example.com is redirected, so we tell libcurl to follow redirection */ 
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    
+        /* Perform the request, res will get the return code */ 
+        res = curl_easy_perform(curl);
+        /* Check for errors */ 
+        if(res != CURLE_OK)
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+    
+        /* always cleanup */ 
+        curl_easy_cleanup(curl);
+    }
+    return 0;
+
+    
 
 // gcc -o dauletTest main.c
 // 
